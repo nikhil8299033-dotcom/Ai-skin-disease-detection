7a4973c2651d1d988c7b9ce250a1554f36638399
NIKHIL SINGH <nikhil8299033@gmail.com>
2026-08-18 00:23:12 +0530

    updated

---
+++ templates/FSD/FSD(CSE-23)/React/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Student from './component/Student'
+
+const App = () => {
+  return (
+    <div>
+      <Student/>
+      <br />
+      <Student/>
+      <br />
+      <Student/>
+      <br />
+    </div>
+  )
+}
+
+export default App
---
+++ templates/FSD/FSD(CSE-23)/React/src/component/Student.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Student = () => {
+  return (
+    <div style={{border:'2px solid red', height : '400px', width:'400px'}}>
+       <h3 style={{color: 'red'}}> student id card</h3>
+       <img src="https://i.pinimg.com/236x/56/76/f1/5676f1b5d20801d9d5d8a03074c515fc.jpg" alt="" height={'150px'} width={'150px'}/>
+       <h3>NIKHIL SINGH</h3>
+       <h3>10 class</h3>
+  
+
+    </div>
+  )
+}
+
+export default Student
